b23a2daef0f3e4856d3a3baceadc1dbcd10c87a0
John Macas <john.macas3@ucuenca.edu.ec>
2024-09-26 16:10:00 -0500

    arreglo del curso

--- sketch.js
+++ sketch.js
@@ -2,12 +2,19 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(24);
   background(0);
-  rectMode(CENTER)
+  rectMode(CENTER);
 }
 function draw() {
-  background(random(150, 200), random(150, 200), 150);
-  fill(mouseX, 255, 120)
-  ellipse(mouseX+80, mouseY-50, mouseX)
-    fill(150, 200, 0)
+  background(random(150, 200), random(150, 200), 150, 20);
+
+  strokeWeight(5);
+noFill()
+  //fill(mousey, 80, 100);
+  stroke(mouseY, 255, 0);
+  ellipse(mouseX + 80, mouseY - 50, mouseX);
+
+  strokeWeight(1);
+  stroke(0, 255, 0);
+  //fill(150, 200, 0);
   rect(mouseX, mouseY, 100, 100);
 }
